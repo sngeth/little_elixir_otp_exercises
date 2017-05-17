574d7fb265f2adf363cf4d508b0352c95e4dc012
Sid Ngeth <sngeth@gmail.com>
2017-05-17 11:08:06 -0400

    How to call erlang library function

---
+++ ex_ch2_4.exs
@@ -0,0 +1,6 @@
+# Translate crypto:md5("Taless from the Crypt"). from Erland to Elixir.
+
+:crypto.md5("Tales from the Crypt") # atom of library named followed by dot operator of function
+# <<79, 132, 235, 77, 3, 224, 121, 88, 98, 75, 61, 67, 62, 16, 233, 91>>
+
+
